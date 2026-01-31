3c35de0304539befe51aba625fb3a5df91f47567
Jahnavi <jahnavi172005@gmail.com>
2026-01-31 19:33:58 +0530

    Update apiClient.js

--- frontend/src/api/apiClient.js
+++ frontend/src/api/apiClient.js
@@ -2,8 +2,13 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: "https://e-commerce-backend-rxb0.onrender.com/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default apiClient;
+
+
 
 
