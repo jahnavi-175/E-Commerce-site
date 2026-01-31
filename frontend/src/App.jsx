4efd07b18ac2ee27e7df7e5095c88cbe1d7ce11a
Jahnavi <jahnavi172005@gmail.com>
2026-01-31 18:40:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,13 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
+import ProductDetail from "./pages/ProductDetail";
 import Cart from "./pages/Cart";
 import Account from "./pages/Account";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Help from "./pages/Help";
 import Checkout from "./pages/Checkout";
-
 
 function App() {
   const isAuthenticated = () => !!localStorage.getItem("token");
@@ -22,11 +22,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
-
             <Route path="/home" element={isAuthenticated() ? <Home /> : <Navigate to="/" />} />
-            <Route path="/cart" element={isAuthenticated() ? <Cart /> : <Navigate to="/" />} />
-            
+            <Route path="/cart" element={isAuthenticated() ? <Cart /> : <Navigate to="/" />} /> 
             <Route path="/all-items" element={isAuthenticated() ? <Product /> : <Navigate to="/" />} />
+            <Route path="/product-detail/:id" element={isAuthenticated() ? <ProductDetail /> : <Navigate to="/" />} />
             <Route path="/account" element={isAuthenticated() ? <Account /> : <Navigate to="/" />} />
             <Route path="/about" element={<About />} />
             <Route path="/contact" element={<Contact />} />
