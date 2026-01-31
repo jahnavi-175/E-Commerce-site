a9981efdfde7be7a224c4bea0218c35fe4a9597f
Jahnavi <jahnavi172005@gmail.com>
2026-01-31 19:09:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(cors());
 
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/orders', require('./routes/order'));
+app.use('/api/products', require('./routes/product'));
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Connected"))
@@ -16,3 +17,4 @@ mongoose.connect(process.env.MONGO_URI)
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+
